39fdcfda5b3cf8e28d6338a93881a5c7a17b0c61
Akira Kato <yo28.5316.akira@gmail.com>
2017-05-27 16:06:23 +0000

    add stats, add validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527114949) do
+ActiveRecord::Schema.define(version: 20170527153350) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
