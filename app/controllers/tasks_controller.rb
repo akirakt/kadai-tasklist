64b13a24a2a4dba3ca8118cfdc0f6b24ee38c59f
Akira Kato <yo28.5316.akira@gmail.com>
2017-06-18 16:10:36 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,23 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:new, :create]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:task] = 'Task が正常に登録されました'
@@ -54,6 +59,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-
-
+  def correct_user
+    redirect_to root_url if @task.user != current_user
+  end
 end
